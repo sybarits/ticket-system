2bb43bb4ba357b353017e8e9e69216679de4e4e7
sybarits <sybarits@gmail.com>
2022-10-17 17:38:00 +0900

    [feat] add config object for service url

---
+++ frontend/src/main/config.js
@@ -0,0 +1,13 @@
+var Config = (function () {
+    const service_url = "http://192.168.137.86:8080/";
+
+    return {
+        getServiceUrl() {
+            return service_url;
+        },
+
+    }
+
+})();
+
+export default Config;
