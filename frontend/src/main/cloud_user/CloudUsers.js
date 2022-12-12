19bd00b8c1cd963538483d22be90a76dc0ab642c
sybarits <sybarits@gmail.com>
2022-12-12 13:39:29 +0900

    [feat] add power user list download feature

--- frontend/src/main/cloud_user/CloudUsers.js
+++ frontend/src/main/cloud_user/CloudUsers.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { Link, useNavigate } from 'react-router-dom';
 import { AgGridReact, AgGridColumn } from "ag-grid-react";
 import "ag-grid-community/dist/styles/ag-grid.css";
@@ -6,7 +6,7 @@ import "ag-grid-community/dist/styles/ag-theme-alpine.css";
 import Stack from '@mui/material/Stack';
 import Button from '@mui/material/Button';
 import axios from 'axios';
-import {utils as XLSXUtils, writeFileXLSX  as writeFileXLSX } from 'xlsx';
+import { read as XLSXread, utils as XLSXutils, writeFileXLSX } from 'xlsx';
 
 import Var from "../Var.js";
 import AuthInfo from "../auth/AuthInfo.js";
@@ -17,6 +17,8 @@ function CloudUsers() {
     const [rowCount, setRowCount] = useState(0);
     const [filteredData, setFilteredData] = useState([]);
     const nevigate = useNavigate();
+    const fileInput = useRef(null);
+    const [powerUserData, setPoserUserData] = useState([]);
 
     const BtnCellRenderer = (e) => {
         return (
@@ -90,17 +92,52 @@ function CloudUsers() {
     };
 
     const saveJsonAsExelFile = (j) => {
-        const ws = XLSXUtils.json_to_sheet(j);
-        const wb = XLSXUtils.book_new();
-        XLSXUtils.book_append_sheet(wb, ws, "Sheet1");
+        const ws = XLSXutils.json_to_sheet(j);
+        const wb = XLSXutils.book_new();
+        XLSXutils.book_append_sheet(wb, ws, "Sheet1");
         writeFileXLSX(wb, "data.xlsx");
     }
 
-    const hendleSaveAsExelButton = () => {
+    const handlePowerUserButton = () => {
+        fileInput.current.click();
+    }
+    const handleInputChange = (e) => {
+        readUserListFile(e);
+    }
+    const readUserListFile = (e) => {
+        const fileReader = new FileReader();
+        const file = e.target.files[0];
+
+        fileReader.onload = async function (event) {
+            let wb = XLSXread(fileReader.result, { type: 'binary', cellDates: true, dateNF: 'YYYY-MM-DD' });
+            let rowObj = XLSXutils.sheet_to_json(wb.Sheets[wb.SheetNames[0]]);
+            for (let i = 0; i < rowObj.length; i++) {
+                await getIBMQUserDataAndSet(rowObj[i]);
+            }
+            saveJsonAsExelFile(rowObj);
+        };
+        fileReader.readAsBinaryString(file);
+    }
+
+    const getIBMQUserDataAndSet = async(u) => {
+        await axios
+            .get(Var.getServiceUrl() + "/user?query_string="+u.User, AuthInfo.getAxiosConfig())
+            .then(({ data }) => {
+                for (let i = 0; i < data.length; i++) {
+                    if (data[i].cloud_service == "IBMQ") {
+                        for(let k of Object.keys(data[i])) {
+                            u[k] = data[i][k];
+                        }
+                    }
+                }
+            });
+    }
+
+    const handleSaveAsExelButton = () => {
         saveJsonAsExelFile(filteredData);
     }
 
-    const hendleAddUserButton = () => {
+    const handleAddUserButton = () => {
         nevigate('/cloud_user_input');
     }
 
@@ -121,8 +158,12 @@ function CloudUsers() {
             <h2>Cloud User Table</h2>
             <Stack spacing={2} direction="row" justifyContent="end" sx={{ m: 1 }}>
                 <Button variant="outlined" onClick={refreshPage}>Refresh</Button>
-                <Button variant="outlined" onClick={hendleAddUserButton}>Add User</Button>
-                <Button variant="outlined" onClick={hendleSaveAsExelButton}>Save As Exel</Button>
+                <Button variant="outlined" onClick={handleAddUserButton}>Add User</Button>
+                <Button variant="outlined" onClick={handleSaveAsExelButton}>Save As Exel</Button>
+                <Button variant="outlined" onClick={handlePowerUserButton}>Power User List</Button>
+            </Stack>
+            <Stack spacing={2} direction="row" justifyContent="center" sx={{ m: 1 }} >
+                <input type="file" accept={".xlsx"} ref={fileInput} onChange={handleInputChange} style={{ display: "none" }} />
             </Stack>
             <div align="left" >Total: {rowCount}</div>
             <div className="ag-theme-alpine" style={{ width: '100%', height: 800, margin: '0 0 0 0' }}>
